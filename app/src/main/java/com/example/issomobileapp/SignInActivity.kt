2afe7025f3897b278a68d66aa471f8f0acd0874f
TalGhar <no.merci420@gmail.com>
2023-04-16 15:42:01 +0300

    guffy ah signing in

--- app/src/main/java/com/example/issomobileapp/SignInActivity.kt
+++ app/src/main/java/com/example/issomobileapp/SignInActivity.kt
@@ -1,11 +1,17 @@
 package com.example.issomobileapp
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.parse.ParseException
+import com.parse.ParseUser
 
 class SignInActivity : AppCompatActivity() {
 
@@ -30,11 +36,44 @@ class SignInActivity : AppCompatActivity() {
             stillNot()
         }
 
+        signInButton.setOnClickListener {
+            signIn(username.text.toString(), password.text.toString())
+        }
+
     }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev != null) {
+            if (ev.action == MotionEvent.ACTION_DOWN) hideKeyBoard()
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+    private fun hideKeyBoard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(window.decorView.windowToken, 0)
+    }
+
 
     private fun stillNot() {
         val intent = Intent(this, SignUpActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun signIn(username: String, password: String) {
+
+        ParseUser.logInInBackground(username, password) { parseUser: ParseUser?, parseException: ParseException? ->
+            if (parseUser != null) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                ParseUser.logOut()
+                if (parseException != null) {
+                    Toast.makeText(this,parseException.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
     }
 
 }
