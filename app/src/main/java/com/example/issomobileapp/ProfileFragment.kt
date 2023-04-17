7b04d8e6c18edcf7d33988c329178cbc958f842d
TalGhar <no.merci420@gmail.com>
2023-04-17 21:53:14 +0300

    added logout feature

--- app/src/main/java/com/example/issomobileapp/ProfileFragment.kt
+++ app/src/main/java/com/example/issomobileapp/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.issomobileapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,13 +9,15 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import com.example.issomobileapp.databinding.FragmentProfileBinding
+import com.parse.ParseException
+import com.parse.ParseUser
 import com.parse.RequestPasswordResetCallback
 import kotlin.math.log
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
-    private lateinit var logOutButton: TextView
+
 
     //region Override Functions
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,12 +39,22 @@ class ProfileFragment : Fragment() {
 
         binding.apply {
             logout.setOnClickListener {
-                Toast.makeText(context, "asd", Toast.LENGTH_LONG).show()
+                logOut()
             }
         }
 
     }
-
     //endregion
+
+    private fun logOut() {
+        ParseUser.logOutInBackground { e: ParseException? ->
+            if (e == null) {
+                val intent = Intent(context, SignInActivity::class.java)
+                startActivity(intent)
+            }
+            else Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+        }
+
+    }
 
 }
