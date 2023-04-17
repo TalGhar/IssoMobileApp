c0fe0345308b4bbaea6e7f5c80a9eece0a94b960
TalGhar <no.merci420@gmail.com>
2023-04-17 21:38:23 +0300

    YOOOOU PONYAL

--- app/src/main/java/com/example/issomobileapp/ProfileFragment.kt
+++ app/src/main/java/com/example/issomobileapp/ProfileFragment.kt
@@ -7,15 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import com.example.issomobileapp.databinding.FragmentProfileBinding
+import com.parse.RequestPasswordResetCallback
+import kotlin.math.log
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var binding: FragmentProfileBinding
     private lateinit var logOutButton: TextView
 
     //region Override Functions
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
     }
 
@@ -24,8 +27,21 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            logout.setOnClickListener {
+                Toast.makeText(context, "asd", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
     //endregion
 
 }
